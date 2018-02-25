89c883861753279eed6530b366ed3d57b87007f9
Divyansh <divi09112@gmail.com>
2018-02-25 19:36:42 +0530

    page with all the courses added

--- moodle/views.py
+++ moodle/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from .models import Course
+from django.views import generic
 
 # Create your views here.
 
 def home(request):
 	'''Home page'''
-	courses=Course.objects.all()
 	return render(request,'home.html',context={'courses':courses})
 
 def courses(request):
 	'''Page with all the courses in which student has enrolled'''
-	courses= Course.objects.filter(student__username='stud1')
+	courses= Course.objects.filter(student__username='stud3')
 	return render( request, 'courses.html' , context={'courses':courses} )
+	
+def allcourses(request):
+	courses=Course.objects.all()
+	return render(request, 'allcourses.html',context={'courses':courses})
