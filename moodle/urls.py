29c1b87b9db7f0e1a33ed6054b24c782cdbaa361
Divyansh <divi09112@gmail.com>
2018-02-25 21:32:42 +0530

    course detail view added

--- moodle/urls.py
+++ moodle/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path('',views.home,name='home'),
 	path('courses/',views.courses,name='courses'),
+	path('courses/(?P<pk>\d+)/',views.courseDetail,name='course-detail'),
 	path('allcourses/',views.allcourses,name='allcourses'),
 ]
--- moodle/views.py
+++ moodle/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Course
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -8,11 +9,20 @@ def home(request):
 	'''Home page'''
 	return render(request,'home.html',context={'courses':courses})
 
+
 def courses(request):
 	'''Page with all the courses in which student has enrolled'''
 	courses= Course.objects.filter(student__username=request.user.username)
 	return render( request, 'courses.html' , context={'courses':courses} )
 	
+	
 def allcourses(request):
 	courses=Course.objects.all()
 	return render(request, 'allcourses.html',context={'courses':courses})
+	
+	
+def courseDetail(request,pk):
+	course = get_object_or_404(Course,pk=pk)
+	
+	if course.student_set.filter(student=request.user.username).exists():
+		return render(request, course_enrolled.html,context={'course':course})
