7e89c74fd765932219c52831390e9837c151c118
Divyansh <divi09112@gmail.com>
2018-02-25 16:06:31 +0530

    made model for courses and user groups professor and student

--- moodle/models.py
+++ moodle/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Course(models.Model):
+	"""Model representing a course"""
+	name = models.CharField(max_length=6,primary_key=True)
+	professor = models.CharField(User,null=False,max_length=100)
+	student =models.ManyToManyField(User)
+
+	def __str__(self):
+		return self.name
