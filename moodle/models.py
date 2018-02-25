9870acac8c523562d049c4ecc62aac23fae3c590
Divyansh <divi09112@gmail.com>
2018-02-25 19:06:05 +0530

    page with courses enrolled in added

--- moodle/models.py
+++ moodle/models.py
@@ -6,7 +6,7 @@ class Course(models.Model):
 	"""Model representing a course"""
 	name = models.CharField(max_length=6,primary_key=True)
 	professor = models.OneToOneField(User,blank=False,null=False,on_delete=models.CASCADE,related_name="+")
-	student =models.ManyToManyField(User)
+	student =models.ManyToManyField(User,null=True,blank=True)
 
 	def __str__(self):
 		return self.name
--- moodle/views.py
+++ moodle/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
+
+def home(request):
+	'''Home page'''
+	courses=Course.objects.all()
+	return render(request,'home.html',context={'courses':courses})
+
+def courses(request):
+	'''Page with all the courses in which student has enrolled'''
+	courses= Course.objects.filter(student__username='stud1')
+	return render( request, 'courses.html' , context={'courses':courses} )
