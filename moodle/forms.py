65b6f2adb3719fc13b42bdf36dce3bfe38f02d69
Divyansh <divi09112@gmail.com>
2018-02-26 18:43:49 +0530

    made a form for professor to add posts

--- moodle/forms.py
+++ moodle/forms.py
@@ -1,4 +1,10 @@
 from django import forms
 
 class NewCourse(forms.Form):
-	name=forms.CharField(required="True",label='Course Name')
+	name=forms.CharField(required=True,label='Course Name')
+	
+
+class NewPost(forms.Form):
+	title=forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={ 'placeholder':'title'}))
+	content=forms.CharField(max_length =500, required =True, widget=forms.TextInput(attrs={ 'placeholder':'post'}))
+	 
