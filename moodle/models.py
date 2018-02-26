f4fc15fbe1fcfa6cb399d3224744b0921371b04e
Divyansh <divi09112@gmail.com>
2018-02-26 16:19:02 +0530

    Made model for messages in feed of a course

--- moodle/models.py
+++ moodle/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+import uuid
 
 # Create your models here.
 
@@ -15,3 +16,13 @@ class Course(models.Model):
 	
 	def get_absolute_url(self):
 		return reverse('course-detail',args=[str(self.name)])
+
+class Messages(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4,)
+	title=models.CharField(max_length=100,blank=False)
+	content=models.CharField(max_length=500,blank=False)
+	course=models.ForeignKey(Course,blank=False,null=False,on_delete=models.CASCADE)
+	timestamp=models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
