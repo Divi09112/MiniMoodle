3466ab4220f22b2751329f39d07077149ea73965
Divyansh <divi09112@gmail.com>
2018-02-27 12:01:12 +0530

    removed common password validator

--- MiniMoodle/settings.py
+++ MiniMoodle/settings.py
@@ -88,9 +88,9 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    'OPTIONS':{'max_similarity':1}},
+    'OPTIONS':{'max_similarity':100}},
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator','OPTIONS':{'min_length':6}
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator','OPTIONS':{'min_length':5}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
