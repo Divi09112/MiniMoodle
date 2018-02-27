7180ccfa4af508778bcd1d934e97f45fa677c916
Divyansh <divi09112@gmail.com>
2018-02-27 11:44:13 +0530

    edited new course form

--- moodle/forms.py
+++ moodle/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class NewCourse(forms.Form):
-	name=forms.CharField(required=True,label='Course Name')
-	
+	name=forms.CharField(required=True,label='Course Name',widget=forms.TextInput(attrs={'maxlength':'6'}))
+	limit = forms.IntegerField(required=True,label='Maximum Strength',widget=forms.NumberInput(attrs={'min':'1'}))
 
 class NewPost(forms.Form):
 	title=forms.CharField(max_length=100,required=True,label='',widget=forms.TextInput(attrs={ 'placeholder':'Title','style':'width:100%;max-width:400px'}))
