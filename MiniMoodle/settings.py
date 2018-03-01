92e0aeaa01d63a78c98531362b5eebae0fe8bd9c
Divyansh <divi09112@gmail.com>
2018-03-01 11:33:44 +0530

    some other changes for deployment

--- MiniMoodle/settings.py
+++ MiniMoodle/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fdx1^h+56#!4uigs@2j(baeu_#fyg)a!*fb%hs-=jv$8$*2ck-'
+#SECRET_KEY = 'fdx1^h+56#!4uigs@2j(baeu_#fyg)a!*fb%hs-=jv$8$*2ck-'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
@@ -71,6 +74,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'MiniMoodle.wsgi.application'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
